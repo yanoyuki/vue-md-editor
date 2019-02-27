92177513f226aef8a49682f636e32f6500503905
nkjmsss <nakajima-sota018@g.ecc.u-tokyo.ac.jp>
2019-02-27 20:42:40 +0900

    change: devide file

--- src/src/plugins/markdown-it/index.js
+++ src/src/plugins/markdown-it/index.js
@@ -29,20 +29,9 @@ markdown
 
 
 // override default renderer
-const defaultRender = markdown.renderer.rules.link_open || function(tokens, idx, options, env, self) {
-  return self.renderToken(tokens, idx, options)
-}
-markdown.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-  const aIndex = tokens[idx].attrIndex('target')
+import { a } from './plugin'
 
-  if (aIndex < 0) {
-    tokens[idx].attrPush(['target', '_blank']) // add new attribute
-  } else {
-    tokens[idx].attrs[aIndex][1] = '_blank'    // replace value of existing attr
-  }
-
-  // pass token to default renderer.
-  return defaultRender(tokens, idx, options, env, self)
-}
+markdown
+  .use(a)
 
 export default markdown
